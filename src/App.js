bf72a8da9caf402ec007d4475a20291686bfebe3
aTomWeaver <atomweavermail@gmail.com>
2023-02-27 07:14:28 -0500

    add cart state to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import RouteSwitch from "./RouteSwitch";
 import Nav from "./components/Nav";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
     <>
       <Nav />
