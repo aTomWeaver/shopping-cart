1ba6222230dd406cc9641b21ea8dbaf69360497c
aTomWeaver <atomweavermail@gmail.com>
2023-02-27 06:39:57 -0500

    product style

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ import '../styles/Product.css'
 function Product({name, color, addToCartHandler}) {
   return (
     <div className="product" style={{background: color, width: "150px"}}>
-      <p>This is a {name} product.</p>
+      <h2>{name}</h2>
       <button onClick={addToCartHandler}>Add To Cart</button>
     </div>
   )
